b4ccd9b74366fe7fe4094b13944b790906b48941
Amir <6641719@gmail.com>
2025-09-25 15:06:42 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "MGPSDK"
+    name: "MPGSDK",
+    defaultLocalization: "ru",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "MPGSDK",
+            targets: ["MPGSDK"]),
+    ],
+    targets: [
+        .target(
+            name: "MPGSDK",
+            dependencies: [
+                .product(
+                    name: "MPGSDK",
+                    package: "gateway-ios-sdk-master")
+            ],
+    ]
 )
